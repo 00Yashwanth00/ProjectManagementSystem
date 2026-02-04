17dd71cf98f85d3a680a5bf24d4b7882dffa1f98
00Yashwanth00 <rameshyashwanth36@gmail.com>
2026-02-04 09:02:07 +0530

    feat: add global exception handling

---
+++ pms/src/main/java/com/yashwanth/pms/common/exception/AccessDeniedException.java
@@ -0,0 +1,7 @@
+package com.yashwanth.pms.common.exception;
+
+public class AccessDeniedException extends RuntimeException{
+    public AccessDeniedException(String message) {
+        super(message);
+    }
+}
